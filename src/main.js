5c5272bc51d574df5731e2dc99dbba4d818c00e1
Edwin Rojas <168507810+Eddu-ER-Tech@users.noreply.github.com>
2024-05-28 13:00:05 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import axios from 'axios';
 import './assets/main.css'
 import 'material-icons/iconfont/material-icons.css'
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000/'
+axios.defaults.baseURL = 'pistigreen-backend.vercel.app'
 
 const app = createApp(App);
 
